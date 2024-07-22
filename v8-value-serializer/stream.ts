aaabe421c2a775ea1e680ea2153e831d4c2e2bb2
Florian Klampfer <mail@qwtel.com>
2024-07-22 16:41:39 +0700

    Commit

--- v8-value-serializer/stream.ts
+++ v8-value-serializer/stream.ts
@@ -1,17 +1,27 @@
 import { Serializer, SerializerOptions, Deserializer, DeserializerOptions } from "./serdes.ts";
 
+/**
+ * A transform stream meant to serialize a multiple JS values over time. 
+ * Each value is double encoded to aid deserialization. This ensures that the byte length is prepended, 
+ * s.t. the deserializer doesn't have to do unnecessary work until enough bytes are buffered, without inventing any new message format.
+ * Values need to piped through the {@link DeserializerStream} to be deserialized.
+ */
 export class SerializerStream extends TransformStream<any, Uint8Array> {
   constructor(options?: SerializerOptions) {
     super({
-      transform(chunk, controller) {
-        const body = new Serializer(options).serialize(chunk);
-        const bodyWrapper = [body];
-        controller.enqueue(new Serializer().serialize(bodyWrapper));
+      transform(value, controller) {
+        const body = new Serializer(options).serialize(value);
+        const chunk = new Serializer().serialize(body);
+        controller.enqueue(chunk);
       },
     });
   }
 }
 
+/**
+ * Meant to deserialize values encoded with the serializer stream. 
+ * It can only be used together with {@link SerializerStream} because it expects a double-encoded format described there.
+ */
 export class DeserializerStream extends TransformStream<Uint8Array, any> {
   constructor(options?: DeserializerOptions) {
     let incompleteBuffer: Uint8Array|null = null;
@@ -23,17 +33,16 @@ export class DeserializerStream extends TransformStream<Uint8Array, any> {
         }
 
         while (true) {
-          if (chunk.byteLength < 12) {
+          if (chunk.byteLength < 5) {
             incompleteBuffer = chunk;
             return;
           }
 
           const deserializer = new Deserializer(chunk, options);
           (deserializer as any).deserializer.suppressDeserializationErrors = true;
-          const bodyWrapper = (deserializer as any).deserializer.readObject();
+          const body = (deserializer as any).deserializer.readObject();
 
-          if (bodyWrapper !== null) {
-            const [body] = bodyWrapper;
+          if (body !== null) {
             const value = new Deserializer(body, options).deserialize();
             controller.enqueue(value);
             chunk = chunk.subarray((deserializer as any).deserializer.position);
