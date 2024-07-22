c2ab87fdef480fb3974c4a0b020f24dbebf34089
Florian Klampfer <mail@qwtel.com>
2024-07-22 20:16:01 +0700

    Commit

--- v8-value-serializer/serdes.ts
+++ v8-value-serializer/serdes.ts
@@ -4,7 +4,7 @@ import {
   ValueDeserializerDelegate, 
   ValueSerializer, 
   ValueSerializerDelegate
-} from "jsr:@workers/v8-value-serializer-core@^0.1.7";
+} from "jsr:@workers/v8-value-serializer-core@^0.1.8";
 
 function copy(source: Uint8Array, dest: Uint8Array, destStart: number, sourceStart: number, sourceEnd: number) {
   dest.set(source.subarray(sourceStart, sourceEnd), destStart);
