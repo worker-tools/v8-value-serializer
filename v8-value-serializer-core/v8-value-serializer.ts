36eda27d5680205331a21529ed2c5ebcfaf904c1
Florian Klampfer <mail@qwtel.com>
2025-05-09 12:58:31 +0200

    Fix

--- v8-value-serializer-core/v8-value-serializer.ts
+++ v8-value-serializer-core/v8-value-serializer.ts
@@ -206,7 +206,7 @@ const SM_MAX = 2 ** 30 - 1; // 2^30 - 1
 const FixedArrayMaxLength = 2 ** 30 - 1
 
 function isSmi(value: number): value is number {
-  return value >= SM_MIN && value <= SM_MAX;
+  return Number.isInteger(value) && value >= SM_MIN && value <= SM_MAX;
 }
 
 function getRegExpFlags(regexp: RegExp): number {
